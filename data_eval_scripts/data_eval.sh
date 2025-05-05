14ac206fe1d479d8a641d75707c18a1ce40f1165
ming <parasolohalo@gmail.com>
2025-05-05 17:16:01 +0800

    Data pipeline code.

---
+++ data_eval_scripts/data_eval.sh
@@ -0,0 +1,10 @@
+HOME_DIR=$(readlink -m ..)
+
+PYTHONPATH=$HOME_DIR python score_by_modality/model_prompter.py \
+  --task $TASK \
+  --image-base $IMAGE_DIR \
+  --input_json $INPUT_JSON \
+  --output_json $OUTPUT_JSON \
+  --instruction_file $HOME_DIR/properties/instructions.json \
+  --model $MODEL \
+  --chat-url $CHAT_SERVICE_URL

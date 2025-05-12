2f746561067a7edd005f5519ef44a4a9b67d902c
ming <parasolohalo@gmail.com>
2025-05-12 22:24:31 +0800

    Update eval jobs code.

--- benchmark_evaluation.sh
+++ benchmark_evaluation.sh
@@ -3,10 +3,16 @@ cd VLMEvalKit
 pip install -e .
 
 pip install lmdeploy openai
+pip install qwen_vl_utils
 
-CUDA_VISIBLE_DEVICES=0 lmdeploy serve api_server "/mnt/hdfs/andrew.estornell/vlm/${MODEL}" --model-name ${MODEL} --server-port 23333  > test-model.log 2>&1 &
+sudo apt-get update
+sudo apt-get install -y libgl1
 
-CUDA_VISIBLE_DEVICES=1 lmdeploy serve api_server "/mnt/hdfs/andrew.estornell/vlm/Qwen2.5-32B-Instruct-AWQ" --model-name Qwen2.5-32B-Instruct-AWQ --server-port 23334 --api-keys sk-123456  > judge-model.log 2>&1 &
+model_name = $(basename "${MODEL}")
+
+CUDA_VISIBLE_DEVICES=0 lmdeploy serve api_server ${MODEL} --model-name ${model_name} --server-port 23333  > test-model.log 2>&1 &
+
+CUDA_VISIBLE_DEVICES=1 lmdeploy serve api_server "Qwen/Qwen2.5-32B-Instruct-AWQ" --model-name Qwen2.5-32B-Instruct-AWQ --server-port 23334 --api-keys sk-123456  > judge-model.log 2>&1 &
 
 cat << 'EOF' >> .env
 LMDEPLOY_API_BASE=http://0.0.0.0:23333/v1/chat/completions
@@ -16,17 +22,25 @@ OPENAI_API_BASE=http://0.0.0.0:23334/v1/chat/completions
 LOCAL_LLM=Qwen2.5-32B-Instruct-AWQ
 EOF
 
-until curl -s http://0.0.0.0:23333/v1/models | grep -q $MODEL; do
+until curl -s http://0.0.0.0:23333/v1/models | grep -q ${model_name}; do
   echo "Waiting for model…"
+  sleep 5
+done
+echo "\n\n\nMODEL LOADED\n\n\n"
+echo "\n\n\nWATING FOR JUDGE\n\n\n"
+
+until curl -s -H "Authorization: Bearer sk-123456" http://0.0.0.0:23334/v1/models | grep -q Qwen2.5-32B-Instruct-AWQ; do
+  echo "Waiting for judge model…"
   sleep 5
 done
 
-until curl -s http://0.0.0.0:23334/v1/models | grep -q $MODEL; do
-  echo "Waiting for model…"
-  sleep 5
-done
+echo "\n\n\nJUDGE LOADED\n\n\n"
+
 
 mkdir -p "/mnt/hdfs/andrew.estornell/vlm/output/${MODEL}"
+mkdir -p "./output/${MODEL}"
 
-python run.py --data ${BENCHMARKS} --model lmdeploy --work-dir "/mnt/hdfs/andrew.estornell/vlm/output/${MODEL}" --verbose --api-nproc 64
+python run.py --data ${BENCHMARKS} --model lmdeploy --work-dir "./output/${MODEL}" --verbose --api-nproc 1
+
+mv "./output/${MODEL}" "/mnt/hdfs/andrew.estornell/vlm/output/${MODEL}"
 
